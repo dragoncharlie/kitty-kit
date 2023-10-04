d48a46cfc2c88f8eb6d863b3e84c78564852c995
DragonCharlie <felinemeowgic@gmail.com>
2023-10-04 10:18:33 +0200

    add storybook

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import Button, {ButtonProps} from "./Button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: "Marbella/Button",
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Primary.args = {
+  title: "Primary",
+};
+
+export const Secondary: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Secondary.args = {
+  title: "Secondary",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+// https://dev.to/abhijitdotsharma/build-and-publish-a-component-library-react-typescript-storybook-34ba
 export * from './components';
