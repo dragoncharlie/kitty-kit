c59dded4464eed9eafce444193795be0e9fafde4
Zhanna Firchuk <22879200+dragoncharlie@users.noreply.github.com>
2023-10-10 03:32:41 -0700

    add jest (#2)
    * add jest
    
    * remove commented lines

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./button.css"
 
-export type ButtonProps = {
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   title: string;
 }
 
-const Button: React.FC<ButtonProps> = ({title}) => {
-  return <button className='kitty-btn'>{title}</button>
+const Button: React.FC<ButtonProps> = ({title, ...other}) => {
+  return <button className='kitty-btn' {...other}>{title}</button>
 }
 
 export default Button;
