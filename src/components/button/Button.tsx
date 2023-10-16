bd572b70f50f41c8a24ca3689709081ed8e13870
DragonCharlie <felinemeowgic@gmail.com>
2023-10-16 16:38:04 +0200

    fix button classes

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 	title: string
+	className?: string
 }
 
-const Button: React.FC<ButtonProps> = ({ title, ...other }) => {
+const Button: React.FC<ButtonProps> = ({ title, className = '', ...other }) => {
 	return (
-		<button className='text-red-500' {...other}>
+		<button className={`text-red-500 ${className}`} {...other}>
 			{title}
 		</button>
 	)
